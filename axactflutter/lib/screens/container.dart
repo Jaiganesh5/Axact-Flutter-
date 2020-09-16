b998b4bf27f65120fbd85261e7f3a5e075a18fba
Jaiganesh <jaiganeshboopathy2000@gmail.com>
2020-09-16 09:38:02 +0530

    "Session 3 container assignment"

--- axactflutter/lib/screens/container.dart
+++ axactflutter/lib/screens/container.dart
@@ -10,6 +10,36 @@ class _DemoState extends State<Demo> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.teal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+            height: 640,
+            width: 100.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.amber,
+                height: 100.0,
+                width: 100.0,
+              ),
+              Container(
+                color: Colors.green,
+                height: 100.0,
+                width: 100.0,
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.blue,
+            height: 640,
+            width: 100.0,
+          )
+        ],
+      ),
     );
   }
 }
